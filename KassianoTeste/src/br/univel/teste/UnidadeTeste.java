851a769c5c56e483707c13b8683ffd2aa5bf6f6d
Kassiano Ferlin <kassianobelem@gmail.com>
2016-03-30 19:56:59 -0300

    Alteração após a criação da tag.

--- KassianoTeste/src/br/univel/teste/UnidadeTeste.java
+++ KassianoTeste/src/br/univel/teste/UnidadeTeste.java
@@ -2,7 +2,8 @@ package br.univel.teste;
 
 public class UnidadeTeste {
 	public static void main(String[] args){
-		String msg = "Hello Word !";
+		String world = "World";
+		String msg = "Hello " + world + "!";
 		System.out.println(msg);
 		
 	}
