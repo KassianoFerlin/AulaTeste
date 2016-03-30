e355e81c5a9263124c6ee761ab58cddd351fe74f
Kassiano Ferlin <kassianobelem@gmail.com>
2016-03-30 20:09:57 -0300

    Voltando ao trabalho na master.

--- KassianoTeste/src/br/univel/teste/UnidadeTeste.java
+++ KassianoTeste/src/br/univel/teste/UnidadeTeste.java
@@ -3,7 +3,8 @@ package br.univel.teste;
 public class UnidadeTeste {
 	public static void main(String[] args){
 		String world = "World";
-		String msg = "Hello " + world + "!";
+		String hello = "Hello ";
+		String msg = hello + world + "!";
 		System.out.println(msg);
 		
 	}
