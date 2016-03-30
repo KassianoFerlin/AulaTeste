b771f075893e6df9db09443ff7bd02e2a9bd3065
Kassiano Ferlin <kassianobelem@gmail.com>
2016-03-30 20:05:56 -0300

    Alteração solicitada pelo cliente.

--- KassianoTeste/src/br/univel/teste/UnidadeTeste.java
+++ KassianoTeste/src/br/univel/teste/UnidadeTeste.java
@@ -3,7 +3,8 @@ package br.univel.teste;
 public class UnidadeTeste {
 	public static void main(String[] args){
 		String msg = "Hello Word !";
+		System.out.println("-----------");
 		System.out.println(msg);
-		
+		System.out.println("-----------");
 	}
 }
