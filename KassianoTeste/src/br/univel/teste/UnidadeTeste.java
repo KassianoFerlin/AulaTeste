537402ec3b092be1a7601239ff4779c674016af6
Kassiano Ferlin <kassianobelem@gmail.com>
2016-03-30 20:46:06 -0300

    Teste 2.2

--- KassianoTeste/src/br/univel/teste/UnidadeTeste.java
+++ KassianoTeste/src/br/univel/teste/UnidadeTeste.java
@@ -4,7 +4,8 @@ public class UnidadeTeste {
 	public static void main(String[] args){
 		String world = "World";
 		String hello = "Hello ";
-		String msg = hello + world + "!";
+		String ha = "HA";
+		String msg = hello + world + ha + "!";
 		System.out.println(msg);
 		
 	}
