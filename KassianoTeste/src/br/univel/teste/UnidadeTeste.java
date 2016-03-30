0b167d454272c07c23a05fd88cf3b54cfc2c819d
Kassiano Ferlin <kassianobelem@gmail.com>
2016-03-30 19:47:35 -0300

    Terceira versão.

--- KassianoTeste/src/br/univel/teste/UnidadeTeste.java
+++ KassianoTeste/src/br/univel/teste/UnidadeTeste.java
@@ -2,7 +2,7 @@ package br.univel.teste;
 
 public class UnidadeTeste {
 	public static void main(String[] args){
-		String msg = "Olá Mundo !";
+		String msg = "Hello Word !";
 		System.out.println(msg);
 		
 	}
